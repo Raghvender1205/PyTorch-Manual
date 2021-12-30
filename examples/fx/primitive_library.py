317588454bce1707a72272ca5c21a2edf283ae0f
Raghvender <raghvender1205@gmail.com>
2021-12-30 19:55:59 +0530

    Inlining Calls and Graph Recompile

--- examples/fx/primitive_library.py
+++ examples/fx/primitive_library.py
@@ -51,3 +51,35 @@ def forward(self, x, y):
     add_lowp = __main___add_lowp(sigmoid_lowp, sigmoid_lowp_1);  sigmoid_lowp = sigmoid_lowp_1 = None
     return add_lowp
 """
+
+# Notice that the calls `sigmoid_lowp` and `add_lowp` appear literally in the trace, they are not traced through
+
+# ********** Inlining Calls **************
+# Define a Function that allows for inlining these calls during graph maipulation
+def inline_lowp_func(n: torch.fx.Node):
+    # If we find a call to a function in our `lowp` module, inline it.
+    if n.op == 'call_function' and n.target.__module__ == inline_lowp_func.__module__:
+        # We want to insert the operations comprising the implementation of the function before the function itself.
+        # Then, we can swap the output value of the function call with the output value for its implementation nodes.
+        with n.graph.inserting_before(n):
+            # We can inline the code by using `fx.Proxy` instances.
+            # `map_arg` traverses all aggregate types and applies the given function to Node instances in data structure.
+            # In this case, we are applying the `fx.Proxy` constructor.
+            proxy_args = torch.fx.node.map_arg(n.args, torch.fx.Proxy)
+            proxy_kwargs = torch.fx.node.map_arg(n.kwargs, torch.fx.Proxy)
+            # Call the function itself with proxy args. This will emit nodes in the graph corresponding to the
+            # operations in the implementation of the function.
+            output_proxy = n.target(*proxy_args, **proxy_kwargs)
+            # Now replace the original node's uses with the output node of the implementation
+            node.replace_all_uses_with(output_proxy.node)
+            # Delete the old node
+            node.graph.erase_node(node)
+
+
+for node in traced.graph.nodes:
+    if node.op == 'call_function' and node.target is sigmoid_lowp:
+        inline_lowp_func(node)
+
+# Recompile after Graph manipulation
+traced.recompile()
+print(traced.code)
