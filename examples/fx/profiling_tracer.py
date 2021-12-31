fc648822920a5dca3f2dda4d3392932b8b5eec5c
Raghvender <raghvender1205@gmail.com>
2021-12-31 13:57:37 +0530

    Using Custom ProfileTracer with special tracer behaviour

--- examples/fx/profiling_tracer.py
+++ examples/fx/profiling_tracer.py
@@ -45,7 +45,7 @@ traced = torch.fx.symbolic_trace(f)
 with torch.autograd.profiler.profile() as prof:
     traced(x)
 
-print(prof)
+#print(prof)
 # "foo" range is not recorded with FX Tracing
 """
 -------------------  ------------  ------------  ------------  ------------  ------------  ------------
@@ -57,4 +57,63 @@ print(prof)
     aten::clamp_min        31.82%       7.000us        31.82%       7.000us       7.000us             1
 -------------------  ------------  ------------  ------------  ------------  ------------  ------------
 Self CPU time total: 22.000us
+"""
+
+class ProfilerTracer(torch.fx.Tracer):
+    def trace(self, root, concrete_args=None):
+        # Original Autograd Profile Record Functions...!!
+        orig_record_function_enter = torch.autograd.profiler.record_function.__enter__
+        orig_record_function_exit = torch.autograd.profiler.record_function.__exit__
+
+        # Fake Profile Record Enter and Exit Functions
+        def fake_profiler_enter(_self):
+            nonlocal self
+            handle_proxy = self.create_proxy(
+                kind='call_function',
+                target=torch.ops.profiler._record_function_enter,
+                args=(_self.name,),
+                kwargs={})
+        
+            assert getattr(_self, '_fx_profiler_ctx', None) is None
+            setattr(_self, '_fx_profiler_ctx', handle_proxy)
+            return handle_proxy
+        
+        def fake_profiler_exit(_self, exc_type, exc_value, traceback):
+            assert hasattr(_self, '_fx_profiler_ctx')
+            handle_proxy = _self._fx_profiler_ctx
+            torch.ops.profiler._record_function_exit(handle_proxy)
+            setattr(_self, '_fx_profiler_ctx', None)
+
+        torch.autograd.profiler.record_function.__enter__ = fake_profiler_enter
+        torch.autograd.profiler.record_function.__exit__ = fake_profiler_exit
+
+        try:
+            return super().trace(root, concrete_args)
+        finally:
+            torch.autograd.profiler.record_function.__enter__ = orig_record_function_enter
+            torch.autograd.profiler.record_function.__exit__ = orig_record_function_exit
+
+
+pt = ProfilerTracer()
+
+graph_with_profiler = pt.trace(f)
+traced_with_profiler = torch.fx.GraphModule(pt.root, graph_with_profiler)
+
+with torch.autograd.profiler.profile() as prof:
+    traced_with_profiler(x)
+
+print(prof)
+# "foo" range is recorded with special tracer behaviour
+"""
+-------------------  ------------  ------------  ------------  ------------  ------------  ------------
+               Name    Self CPU %      Self CPU   CPU total %     CPU total  CPU time avg    # of Calls
+-------------------  ------------  ------------  ------------  ------------  ------------  ------------
+                foo        51.35%      19.000us       100.00%      37.000us      37.000us             1
+        aten::empty        10.81%       4.000us        10.81%       4.000us       4.000us             1
+         aten::relu         8.11%       3.000us        37.84%      14.000us      14.000us             1
+    aten::clamp_min        10.81%       4.000us        29.73%      11.000us      11.000us             1
+        aten::empty         2.70%       1.000us         2.70%       1.000us       1.000us             1
+    aten::clamp_min        16.22%       6.000us        16.22%       6.000us       6.000us             1
+-------------------  ------------  ------------  ------------  ------------  ------------  ------------
+Self CPU time total: 37.000us
 """
